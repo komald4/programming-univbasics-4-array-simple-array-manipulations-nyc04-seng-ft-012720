53227ebec72a52c02a615315462a1755902aefdf
calm-fork-7535 <komald.4@gmail.com>
2020-01-17 19:14:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,5 @@
-def colors_in_the_rainbow [array, elements]
+def colors_in_the_rainbow (array, elements)
+  colors_in_the_rainbow[]
   next_color = "violet"
   colors_in_the_rainbow.push(next_color)
 end
