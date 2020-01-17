55cc4548dcf755019f6445a6ecdd6116e95c7def
calm-fork-7535 <komald.4@gmail.com>
2020-01-17 19:25:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,9 @@ def using_push(array, elements)
   next_color = "violet"
   colors_in_the_rainbow.push(next_color)
 end
+
+def using_unshift(array, elements)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
+  colors_in_the_rainbow.unshift(next_color)
+end
