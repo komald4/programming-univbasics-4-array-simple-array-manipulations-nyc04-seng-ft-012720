e062a06309558bcfda4e2c70e5ef064f807f2fd9
calm-fork-7535 <komald.4@gmail.com>
2020-01-17 19:25:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,6 +6,6 @@ end
 
 def using_unshift(array, elements)
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-  next_color = "violet"
+  next_color = "Staten Island"
   colors_in_the_rainbow.unshift(next_color)
 end
