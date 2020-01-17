ea65d353122f15e77bf8225eca96596d20fe2427
calm-fork-7535 <komald.4@gmail.com>
2020-01-17 19:15:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def pop(array, elements)
-  colors_in_the_rainbow[]
+  colors_in_the_rainbow["red", "orange", "yellow", "green", "blue", "indigo"]
   next_color = "violet"
   colors_in_the_rainbow.push(next_color)
 end
