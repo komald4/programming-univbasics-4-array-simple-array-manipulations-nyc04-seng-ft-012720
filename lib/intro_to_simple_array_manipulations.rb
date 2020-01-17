331a4f2101385615121d7749781617ebce572539
calm-fork-7535 <komald.4@gmail.com>
2020-01-17 19:17:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,4 @@
-def pop(array, elements)
+def pop()
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
   next_color = "violet"
   colors_in_the_rainbow.push(next_color)
