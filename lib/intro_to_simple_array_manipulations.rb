1515b29664dc1c22f9fa7b21ae5fc437a1c74ac0
calm-fork-7535 <komald.4@gmail.com>
2020-01-17 19:20:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,4 @@
 def push(array, string)
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-  next_color = "violet"
-  colors_in_the_rainbow.push(next_color)
+  colors_in_the_rainbow.push("violet")
 end
