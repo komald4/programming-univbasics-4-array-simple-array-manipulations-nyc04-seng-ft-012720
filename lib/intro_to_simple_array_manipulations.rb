d0d0f418d2cd7b2886831dc58f58f91bfe807df2
calm-fork-7535 <komald.4@gmail.com>
2020-01-17 20:14:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -40,10 +40,19 @@ def using_concat(array1, array2)
 array1 = array1.push(*array2)
 end
 
+
+
 def using_insert(array, element)
 array = array.insert(4,element)
 end
 
+
+
 def using_uniq(array)
   array = array.uniq
+end
+
+
+def using_flatten(array)
+  array = array.flatten
 end
